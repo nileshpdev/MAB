569072341dd8d78830bad299633e4f3819b52b40
nileshgp <nileshgpatel@outlook.com>
2022-07-05 08:55:41 +0100

    raw dom elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { InfoComponent } from './info/info.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddressComponent } from './address/address.component';
+import { MaterialUi } from '../material.module';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ResultsComponent } from './results/results.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoComponent,
+    ContactComponent,
+    AddressComponent,
+    ResultsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MaterialUi,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/info/info.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info',
+  templateUrl: './info.component.html',
+  styleUrls: ['./info.component.scss']
+})
+export class InfoComponent implements OnInit {
+  infoText = `This is just some placeholder text about scrabble. 
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do iudtempor incidunt ut labore et dolore magna aliqua`;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
